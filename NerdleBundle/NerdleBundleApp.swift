541065612bd372a9726767c0c0e3f3a67b8bb1a5
vfil0001 <vfil0001@student.monash.edu>
2025-10-08 03:14:06 +1100

    Initial Commit

---
+++ NerdleBundle/NerdleBundleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NerdleBundleApp.swift
+//  NerdleBundle
+//
+//  Created by V on 8/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct NerdleBundleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
